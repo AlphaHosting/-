f38e641c2413fb733c8f5bd6a50bd3824385cacd
AlphaHosting <42865213+AlphaHosting@users.noreply.github.com>
2018-09-24 18:21:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,7 +94,7 @@ client.on('message', message => {
   if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
   let args = message.content.split(' ').slice(1).join(' ');
-  let copy = "FMGR STAFF";
+  let copy = "By BlackFire#3636";
   let request = `Requested By ${message.author.username}`;
   if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
   
